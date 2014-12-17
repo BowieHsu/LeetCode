4faa565e8d3c857c271fce28ecba4a1fac8949da
BowenXu <bowiehsu1024@gmail.com>
2014-12-17 14:55:51 +0800

    4sum

---
+++ 4sum.cpp
@@ -0,0 +1,21 @@
+//
+//  4sum.cpp
+//  Longest Substring
+//
+//  Created by Bowie Hsu  on 14/12/17.
+//  Copyright (c) 2014年 Bowie Hsu . All rights reserved.
+//
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int> > fourSum(vector<int> &num, int target)
+    {
+        vector<vector<int>> output;
+        vector<int> buffer;
+        //对输入序列进行排序
+        sort(num.begin(),num.end());
+        
+    }
+};

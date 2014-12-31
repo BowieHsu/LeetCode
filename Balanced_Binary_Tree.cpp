3faa38eb8d208d6d497805ddb6fec62bc400c8f5
BowenXu <bowiehsu1024@gmail.com>
2014-12-31 23:24:46 +0800

    easy

--- Balanced_Binary_Tree.cpp
+++ Balanced_Binary_Tree.cpp
@@ -20,7 +20,23 @@
 using namespace std;
 class Solution {
 public:
-    bool isBalanced(TreeNode *root) {
-        
+    //递归返回树的高度
+    //因为result结果是要传回到isBanlanced函数中，所以是引用
+    int judge(TreeNode *root,bool &result)
+    {
+        if (!root)return 0;
+        int leftheight=judge(root->left, result);
+        int rightheight=judge(root->right, result);
+        if (abs(leftheight-rightheight)>1)
+            result=false;
+        return max(leftheight,rightheight)+1;
     }
+    
+    bool isBalanced(TreeNode *root)
+    {
+        bool result=true;
+        int height=judge(root, result);
+        return result;
+    }
+  
 };

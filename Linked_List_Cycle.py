3f3774e6e2fbee1e0b0ac2fd22630bf571c7405a
BowenXu <hjc336@163.com>
2015-02-07 15:25:09 +0800

    update 2.7

--- Linked_List_Cycle.py
+++ Linked_List_Cycle.py
@@ -9,3 +9,22 @@ class Solution:
     # @param head, a ListNode
     # @return a boolean
     def hasCycle(self, head):
+        if head == None:
+            return False
+        fast = head
+        slow = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+instance = Solution()
+Node1,Node2,Node3 = ListNode(1),ListNode(2),ListNode(3)
+Node = ListNode(0)
+head = ListNode(1)
+Node.next,Node1.next,Node2.next = Node1,Node2,Node
+r = instance.hasCycle(head)
+print r
+print Node

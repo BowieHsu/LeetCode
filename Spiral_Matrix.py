d6a5b0b072cdabefddc02a67358983f31a715e8d
BowenXu <hjc336@163.com>
2015-02-14 09:39:59 +0800

    update 2.14

--- Spiral_Matrix.py
+++ Spiral_Matrix.py
@@ -1,19 +1,42 @@
 __author__ = 'bowiehsu'
 class Solution:
-    def spiralOrder(self,matrix):
-        out = matrix
-        trans = []
-        i,j = 0,0
-        m,n = len(matrix),len(matrix[0])
-        for v in matrix:
-            trans += v
-        while j < m and i == 0:
-             out[i][j] = trans[]
+    def generateMatrix(self,n):
+        trans,out = [],[]
+        for i in range(n):
+            out.append(range(n))
+        #print out
+        trans.extend(range(1,n**2+1))
+        l,j,i = 0,0,0
+        box,boy,upx,upy = 0,0,n-1,n-1
+        while box <= upx and boy <= upy:
+          #向右
+          while j < upy and i == box:
+             out[i][j] = trans[l]
+             l += 1
              j += 1
-             #break
-
+          box += 1
+          #向下
+          while j == upy and i < upx:
+             out[i][j] = trans[l]
+             i += 1
+             l += 1
+          upy -= 1
+          #向左
+          while i == upx and j > boy:
+             print l
+             out[i][j] = trans[l]
+             j -= 1
+             l += 1
+          upx -= 1
+          #向上
+          while j == boy and i > box :
+             out[i][j] = trans[l]
+             i -= 1
+             l += 1
+          boy += 1
+          #每次单向移动完之后修改边界条件，由于界定条件原因，最后一个值单独赋
+          out[i][j] = trans[l]
         return out
 
 insatcne = Solution()
-r = [[1,2,3],[4,5,6],[7,8,9]]
-print insatcne.spiralOrder(r)
+print insatcne.generateMatrix(3)

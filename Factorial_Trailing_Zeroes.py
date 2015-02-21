0d9cca925e6bca4d2a6619b900ed69113584ab04
BowieHsu <hjc336@163.com>
2015-02-21 17:13:39 +0800

    update 2.21.2

--- Factorial_Trailing_Zeroes.py
+++ Factorial_Trailing_Zeroes.py
@@ -1,19 +1,8 @@
 __author__ = 'bowiehsu'
 class Solution:
     def trailingZeroes(self, n):
-        num = self.generate(n)
-        out = list(str(num))
-        out.sort()
-        ras = 0
-        for i in out:
-            if i == '0':
-                ras += 1
-        return ras
-    def generate(self,n):
-        if n == 0:
-            return 1
-        else:
-            return n*self.generate(n-1)
+        return  0 if n < 5 else n/5 + self.trailingZeroes(n/5)
 
 instance = Solution()
-print instance.generate(500)
+print 8/5
+print instance.trailingZeroes(500)

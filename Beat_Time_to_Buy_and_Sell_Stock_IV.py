a27b30f62d0750496e9c48ba2226e87df1951059
BowieHsu <hjc336@163.com>
2015-03-18 13:39:34 +0800

    Update 18th.Mar

---
+++ Beat_Time_to_Buy_and_Sell_Stock_IV.py
@@ -0,0 +1,19 @@
+#encoding:utf-8
+__author__ = 'bowiehsu'
+class Solution:
+    # @return an integer as the maximum profit
+    def maxProfit(self,k,prices):
+        #思想是求第二次卖出之后的最大值
+        hold = [-65535]*k
+        profit = [0]*k
+        for i in prices:
+            while k:
+                profit[k-1] = max(profit[k-1], hold[k-1]+i)
+                hold[k-1] = max(hold[k-1],-i)
+
+                k -= 1
+        return profit
+
+instance = Solution()
+prices = [1,2,3,4,5]
+print instance.maxProfit(2,prices)
---
+++ lru-cache.py
@@ -0,0 +1,17 @@
+#encoding:utf-8
+__author__ = 'bowiehsu'
+
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+
+
+    # @return an integer
+    def get(self, key):
+
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
